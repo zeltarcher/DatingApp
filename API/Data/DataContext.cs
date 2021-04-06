9a3cd107956f7428943d99cc1a99aeca62a229b4
zeltarcher <truongchiquang@gmail.com>
2021-04-05 20:57:16 -0500

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+        
+    }
+}
